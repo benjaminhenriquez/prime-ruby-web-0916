5c75bf3b041716c6ab98c0824320c7b08b8fa8d7
Benjamin Henriquez <henriquez.ben@gmail.com>
2016-09-16 07:00:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  if num == 2
+    true
+  elsif num < 2
+    false
+  else
+      count = 2
+      until count == (num-1) do
+          if num%count == 0
+              return false
+          elsif num%count>0
+              count+=1
+          end
+      end
+      true
+  end
+  end
